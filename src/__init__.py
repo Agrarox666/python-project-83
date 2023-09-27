2d52492ddac51dd5dc73b6ad609d59e74491b3fc
Igor Loskutov <omfi017@gmail.com>
2023-09-27 17:02:18 +0500

    addition of input form and its validation

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
+import os
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__, template_folder='../templates')
 __all__ = app  # Доступ на уровне пакета
+#DATABASE_URL = os.getenv('DATABASE_URL')
+#connection = psycopg2.connect(DATABASE_URL)
--- src/page_analyzer.py
+++ src/page_analyzer.py
@@ -1,5 +1,6 @@
 from src import app
-from flask import render_template
+from flask import render_template, request, redirect
+from validators import url
 
 app = app
 
@@ -10,6 +11,24 @@ def handler():
         'index.html')
 
 
-@app.route('/show/<ids>')
-def get_show(ids):
-    return f'Show me your{ids}'
+@app.route('/urls', methods=['POST'])
+def handler_form():
+    input_url = request.form.to_dict()['url']
+    error = validate_url(input_url)
+    if error:
+        return render_template(
+            'index_error.html'
+        )
+    return f'It will be url(id) here. Current url is {input_url}'
+
+
+@app.route('/urls')
+def show_urls():
+    return 'It will be urls here'
+
+
+def validate_url(input_url):
+    if url(input_url):
+        return False
+    else:
+        return True
